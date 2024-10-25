e051577b6b3fab019e6ceec3c20a4b358ce47e8b
Ренат Асланов <renat@Renats-MacBook-Air.local>
2024-10-25 14:44:24 +0300

    deleted files for rorganization project
     Please enter the commit message for your changes. Lines starting

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <QApplication>
 #include <QPushButton>
-#include "headers/Vector2D.h"
-#include "headers/mainwindow.h"
-#include "headers/ObjReader.h"
+// #include "headers/Vector2D.h"
+// #include "headers/mainwindow.h"
+// #include "headers/ObjReader.h"
 
 int main(int argc, char *argv[]) {
-    //todo Читает только до 41659 файла почему-то
-    std::string filename("/Users/renat/IdeaProjects/CompGraphProject/3DModels/Faceform/WrapFemaleBody.obj");
-    // std::string filename("/Users/renat/IdeaProjects/CompGraphProject/3DModels/CaracalCube/caracal_cube.obj");
-    Model ans(ObjReader::read(filename));
+    // //todo Читает только до 41659 файла почему-то
+    // std::string filename("/Users/renat/IdeaProjects/CompGraphProject/3DModels/Faceform/WrapFemaleBody.obj");
+    // // std::string filename("/Users/renat/IdeaProjects/CompGraphProject/3DModels/CaracalCube/caracal_cube.obj");
+    // Model ans(ObjReader::read(filename));
+    //
+    // QApplication a(argc, argv);
+    // MainWindow w;
+    // w.show();
+    // // QPushButton button(nullptr);
+    // // button.resize(200, 100);
+    // // button.show();
 
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    // QPushButton button(nullptr);
-    // button.resize(200, 100);
-    // button.show();
-
-    return a.exec();
+    // return a.exec();
 }
