57bfe090c3c7af6f17538383fb668d60a8f58d7f
Ренат Асланов <renat@Renats-MacBook-Air.local>
2024-10-25 16:41:49 +0300

    delet all

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_CompGraph_FILE /Users/renat/CLionProjects/CompGraph/cmake-build-debug/CompGraph)
-set(__QT_DEPLOY_TARGET_CompGraph_TYPE EXECUTABLE)
--- main.cpp
+++ main.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 #include <QApplication>
 #include <QPushButton>
-// #include "headers/Vector2D.h"
-// #include "headers/mainwindow.h"
-// #include "headers/ObjReader.h"
+#include "headers/Vector2D.h"
+#include "headers/mainwindow.h"
+#include "headers/ObjReader.h"
 
 int main(int argc, char *argv[]) {
-    // //todo Читает только до 41659 файла почему-то
-    // std::string filename("/Users/renat/IdeaProjects/CompGraphProject/3DModels/Faceform/WrapFemaleBody.obj");
-    // // std::string filename("/Users/renat/IdeaProjects/CompGraphProject/3DModels/CaracalCube/caracal_cube.obj");
-    // Model ans(ObjReader::read(filename));
-    //
-    // QApplication a(argc, argv);
-    // MainWindow w;
-    // w.show();
-    // // QPushButton button(nullptr);
-    // // button.resize(200, 100);
-    // // button.show();
+    //todo Читает только до 41659 файла почему-то
+    std::string filename("/Users/renat/IdeaProjects/CompGraphProject/3DModels/Faceform/WrapFemaleBody.obj");
+    // std::string filename("/Users/renat/IdeaProjects/CompGraphProject/3DModels/CaracalCube/caracal_cube.obj");
+    Model ans(ObjReader::read(filename));
 
-    // return a.exec();
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    // QPushButton button(nullptr);
+    // button.resize(200, 100);
+    // button.resize(200, 100);∂
+    // button.show();
+
+    return a.exec();
 }
