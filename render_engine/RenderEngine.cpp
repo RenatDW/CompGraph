6aeb845743d02f7160c184a09e188ffe0866154a
Ренат Асланов <aslanovrenat7@gmail.com>
2024-10-29 22:34:15 +0200

    deleted redudant comments

--- render_engine/RenderEngine.cpp
+++ render_engine/RenderEngine.cpp
@@ -3,32 +3,6 @@
 #include "../headers/mainwindow.h"
 #include "../headers/Point2D.h"
 
-// #include <QPainter>
-// void RenderEngine::render(QPainter& painter){
-// const Camera camera,
-// const Model mesh,
-// const int width,
-// const int height,
-// const bool showTriangulation) {
-// Matrix4f modelMatrix = rotateScaleTranslate();
-// Matrix4f viewMatrix = camera.getViewMatrix();
-// Matrix4f projectionMatrix = camera.getProjectionMatrix();
-//
-// Matrix4f modelViewProjectionMatrix = new Matrix4f(modelMatrix);
-// modelViewProjectionMatrix.mul(viewMatrix);
-// modelViewProjectionMatrix.mul(projectionMatrix);
-// if(showTriangulation.equals(ShowTriangles.TRIANGLES)) {
-//     renderTriangle(graphicsContext, mesh, width, height, modelViewProjectionMatrix);
-// } else
-//     renderPolygons(graphicsContext, mesh, width, height, modelViewProjectionMatrix);
-
-// }
-
-// void RenderEngine::render(QPainter painter) {
-//     painter.drawLine(10,10,200,200);
-// }
-
-
 void RenderEngine::render(QPainter &painter,
                           Camera &camera,
                           const Model &mesh,
