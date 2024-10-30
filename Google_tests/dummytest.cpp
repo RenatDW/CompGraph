eeb621db19dfc1478e1c98e2d7ad33428f0de2d9
Mathew <mathwkit@gmail.com>
2024-10-30 23:15:50 +0300

    second dummy test

--- Google_tests/dummytest.cpp
+++ Google_tests/dummytest.cpp
@@ -1,19 +1,5 @@
+//
+// Created by User on 30.10.2024.
+//
+
 #include "gtest/gtest.h"
-
-#include "../objwriter/dummyWriter.cpp"
-
-TEST(SumTest, HandlesPositiveNumbers) {
-    EXPECT_FLOAT_EQ(sum(1.0f, 2.0f), 3.0f); // Проверяем сумму положительных чисел
-}
-
-TEST(SumTest, HandlesNegativeNumbers) {
-    EXPECT_FLOAT_EQ(sum(-1.0f, -2.0f), -3.0f); // Проверяем сумму отрицательных чисел
-}
-
-TEST(SumTest, HandlesMixedNumbers) {
-    EXPECT_FLOAT_EQ(sum(1.0f, -2.0f), -1.0f); // Проверяем сумму смешанных чисел
-}
-
-TEST(SumTest, HandlesZero) {
-    EXPECT_FLOAT_EQ(sum(0.0f, 0.0f), 0.0f); // Проверяем сумму нулей
-}
