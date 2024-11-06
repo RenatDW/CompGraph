90f0cf3cd24c8d094e00455c870761c0960a440c
Mathew <mathwkit@gmail.com>
2024-11-07 01:13:05 +0300

    fix poin3d

--- math/src/Point3D.cpp
+++ math/src/Point3D.cpp
@@ -1,75 +1,74 @@
-//
-// Created by Ренат Асланов on 03.11.2024.
-//
-
 #include "../headers/Point3D.h"
 #include "../headers/Vector3D.h"
-Point3D::Point3D(float x, float y, float z)
+
+#include <cmath>
+
+Point3D::Point3D(const float x, const float y, const float z)
 {
     this->x = x;
     this->y = y;
     this->z = z;
 }
 
-float Point3D::getX()
+float Point3D::getX() const
 {
     return x;
 }
 
-float Point3D::getY()
+float Point3D::getY() const
 {
     return y;
 }
 
-float Point3D::getZ()
+float Point3D::getZ() const
 {
     return z;
 }
 
-void Point3D::setX(float x)
+void Point3D::setX(const float x)
 {
     this->x = x;
 }
 
-void Point3D::setY(float y)
+void Point3D::setY(const float y)
 {
     this->y = y;
 }
 
-void Point3D::setZ(float z)
+void Point3D::setZ(const float z)
 {
     this->z = z;
 }
 
-void Point3D::set(float x, float y, float z)
+void Point3D::set(const float x, const float y, const float z)
 {
     this->x = x;
     this->y = y;
     this->z = z;
 }
 
-Point3D Point3D::operator*(const float a) const
+Point3D Point3D::operator * (const float a) const
 {
     return {this->x * a, this->y * a, this->z * a};
 }
 
-Point3D Point3D::operator+(Point3D &v2) const
+Point3D Point3D::operator + (const Point3D &v2) const
 {
     return {this->x + v2.x, this->y + v2.y, this->z + v2.z};
 }
 
-Point3D Point3D::normalize()
+Point3D Point3D::normalize() const
 {
-    float len = sqrt(this->x * this->x + this->y * this->y + this->z * this->z);
+    const float len = std::sqrt(this->x * this->x + this->y * this->y + this->z * this->z);
     return  {this->x / len, this->y / len, this->z / len};
 }
 
-Point3D Point3D::operator-(const Point3D &point_3d) const
+Point3D Point3D::operator - (const Point3D &point_3d) const
 {
     return {this->x - point_3d.x, this->y - point_3d.y, this->z - point_3d.z};
 }
 
-float Point3D::dot(Point3D &v)
+float Point3D::dot(const Point3D &v) const
 {
     return this->x * v.getX() + this->y * v.getY() + this->z * v.getZ();
 
@@ -81,12 +80,12 @@ Point3D Point3D::operator*(const Point3D &cam) const
 }
 
 
-Point3D Point3D::vertex_to_point(Vector3D vertex, int width, int height, float near, float far)
+Point3D Point3D::vertex_to_point(const Vector3D& vertex, const int width, const int height, const float near, const float far)
 {
-    return Point3D(vertex.getX() * width + width / 2.0F, -vertex.getY() * height + height / 2.0F, (vertex.getZ() - near) / (far - near));
+    return {vertex.getX() * static_cast<float>(width) + static_cast<float>(width) / 2.0f, -vertex.getY() * static_cast<float>(height) + static_cast<float>(height) / 2.0f, (vertex.getZ() - near) / (far - near)};
 }
 
-Vector3D Point3D::point_to_vector(Point3D point)
+Vector3D Point3D::point_to_vector(const Point3D point)
 {
-    return Vector3D(point.getX(), point.getY(), point.getZ());
+    return {point.getX(), point.getY(), point.getZ()};
 }
