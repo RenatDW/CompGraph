71fdc9a6d46ed609460390c44a38ceb8d370e983
renat <aslanovrenat7@gmail.com>
2024-12-19 13:59:06 +0300

    Refactoring in mainwindow.cpp

--- src/forms/mainwindow.cpp
+++ src/forms/mainwindow.cpp
@@ -525,21 +525,6 @@ void MainWindow::on_checkBox_show_illumination_toggled(bool checked)
     // QMessageBox::information(this, "Save model", "Today is tuesday");
 }
 
-//void MainWindow::wheelEvent(QWheelEvent *event) { ТРУП КОЛЕСИКА МЫШИ ХАПХВАХПХАВХВХАПАВХАХП
-//	int delta = event->angleDelta().y();
-//	const float zoom_speed = 0.1f;
-//	camera.move_position(Vector3D(0, 0, static_cast<float>(delta) * zoom_speed * (-1)));
-//
-//	std::string ans = "{" + std::to_string(camera.get_position().getX()) + ", " + std::to_string(camera.get_position().getY()) + ", " + std::to_string(
-//			camera.get_position().getZ()) + "}";
-//	ui->listWidget_2->item(selected_camera_list_id)->setText(QString::fromStdString(ans));
-//	QVariant cord;
-//	std::array<float, 4> coord { camera.get_position().getX(), camera.get_position().getY(), camera.get_position().getZ(), static_cast<float>(selected_camera_model_id)};
-//	cord.setValue(coord);
-//	ui->listWidget_2->item(selected_camera_list_id)->setData(Qt::UserRole, cord);
-//
-//	update_scene();
-//}
 void MainWindow::on_pushButton_5_clicked()
 {
 	if(ui->pushButton_5->isEnabled())
