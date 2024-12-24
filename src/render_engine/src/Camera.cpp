c79a80bddd33218ac632acfa627875284cd881ae
Mathew <mathwkit@gmail.com>
2024-12-24 17:02:40 +0300

    fixed mousemoveevent

--- src/render_engine/src/Camera.cpp
+++ src/render_engine/src/Camera.cpp
@@ -65,8 +65,5 @@ Matrix4D Camera::get_projection_matrix() const
 {
     return GraphicConveyor::perspective(fov, aspectRatio, nearPlane, farPlane);
 }
-Camera::Camera()
-{
-
-};
+Camera::Camera() = default;
 
